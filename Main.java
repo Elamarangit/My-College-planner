901f5d3ebb1ff9a6283f735f8dc8de0a56e8ea57
Saurya-pixel <sauri.bachala@gmail.com>
2025-05-23 16:26:06 +0000

    Main deposit function

--- Main.java
+++ Main.java
@@ -16,16 +16,24 @@ public class Main {
         BankAccount newUser = new BankAccount(userName, amountInbank);
         System.out.println("Hello "+newUser.getAccHolder()+", let's get you started!");
         boolean codeRunning = true;
-        System.out.println("Which college are you interested in? If you would like to see your balance: please type 'balance' or type 'e' to exit");
+        System.out.println("Which college are you interested in? If you would like to see your balance: please type 'balance'" + "\n" +"Type 'deposit' if you want to add more money to your account" +
+        "\n" + "Type 'e' to exit");
         while(codeRunning){
             String response = in.nextLine();
             if(response.equals("balance") || response.equals("Balance")){
                 System.out.println(newUser.toString());
             }
-            if(response.equals("e") || response.equals("E")){
+            else if(response.equals("e") || response.equals("E")){
                 System.out.println("Good luck for college!");
                 codeRunning = false;
-            }       
+            }   
+            else if(response.equals("deposit") || response.equals("Deposit")){
+                System.out.println("How much would you like you to deposit?");
+                double depositInput = in.nextInt();
+                newUser.deposit(depositInput);
+                System.out.println("You have deposited " + "$" + depositInput + " into your account!");
+                System.out.println("Which collegeare you interested in? If you would like to see your balance: please type 'balance'" + "\n" +"Type 'deposit' if you want to add more money to your account" + "\n" + "Type 'e' to exit");
+            } 
             
 
             
